1caee8a589b7a534c6fea53cc36181bfd07d1423
unknown <tkfydtls464@gmail.com>
2022-07-27 10:11:15 +0900

    1 solve

--- 6주차 선형 자료구조/1021번.py
+++ 6주차 선형 자료구조/1021번.py
@@ -7,18 +7,19 @@ location = list(map(int, sys.stdin.readline().split()))
 list_pre = list(range(1, N+1))
 que = deque(list_pre)
 cnt = 0
-while location:
+while len(que) > N-M:
     for x in range(M):
-        if location[x] > N/2:
+        if que.index(location[x]) > len(que)/2:
             while que[0] != location[x]:
                 y = que.pop()
                 que.appendleft(y)
-                cnt +=1
+                cnt += 1
+            que.popleft()
+
         else:
             while que[0] != location[x]:
-                print(que[0], location[x], x)
                 z = que.popleft()
                 que.append(z)
-                cnt +=1
-        que.popleft()
+                cnt += 1
+            que.popleft()
 print(cnt)
