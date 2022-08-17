605c0d825fe8e4d372090ede431aeb365710d323
unknown <tkfydtls464@gmail.com>
2022-08-17 22:11:01 +0900

    1 solved

--- 11주차 이분탐색/2805번.py
+++ 11주차 이분탐색/2805번.py
@@ -2,31 +2,19 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 tree = list(map(int, sys.stdin.readline().split()))
-tree.sort()
 
-if N == 1:
-    print(tree[0]-M)
-    sys.exit(0)
+start, end = 1, max(tree)
 
-cnt = 0
-if M > tree[-1]:
-    start, end = tree[-1], M
-    left, right = 0, M - tree[-1]
-else:
-    start, end = M, tree[-1]
-    left, right = 0, tree[-1] - M
+while start <= end:
+    mid = (start + end) // 2
 
-i = (left+right)//2
-
-while cnt != M:
     cnt = 0
-    cut = pre[i]
     for tre in tree:
-        if tre > cut:
-            cnt += tre - cut
-    if cnt > M:
-        i = (right + i) // 2
-    else:
-        i = (left + i) // 2
+        if tre >= mid:
+            cnt += tre - mid
 
-print(cut)
+    if cnt >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
