8c9a889e7bc4eff47d4607b71d08619e2488e0c2
unknown <tkfydtls464@gmail.com>
2023-03-24 21:27:02 +0900

    1 solved

--- JavaScript/백준/2주차-dfs,bfs/2828.js
+++ JavaScript/백준/2주차-dfs,bfs/2828.js
@@ -1,22 +1,25 @@
 const fs = require('fs');
-//const inpu = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
-let inpu = fs.readFileSync("input.txt").toString().trim().split('\r\n');
+const inpu = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
 
-const NM = inpu[0].split(' ').map(a=>Number(a))
-const N = NM[0]
-const M = NM[1]
-const J = Number(inpu[2])
-const apple = inpu.slice(2,inpu.length).map(a=>a-1)
+const [N,M] = inpu[0].split(' ').map(a=>Number(a))
+const J = Number(inpu[1])
+const apples = inpu.slice(2,inpu.length).map(a=> Number(a))
 
 let answer = 0
-let bucket = [...Array(M).keys()]
-for(let j=0; j<J; j++){
-    for(let i=0;i<N;i++){
-        if(apple[j] in bucket){
-            answer += 1
-            break
-        }
-        bucket.map(a=>a+1)
+let start = 1
+let end = M
+
+for(let apple of apples){
+    if (end < apple){
+        answer += apple - end
+        end = apple
+        start = apple - (M - 1)
+    }
+
+    else if (start > apple){
+        answer += start - apple
+        start = apple
+        end = apple + (M - 1)
     }
 }
 
